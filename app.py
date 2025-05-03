0a30926cf82fff65909367e6593eff487f900a2f
Vaishnavi-Appasi <appasivaishnavi@gmail.com>
2025-05-03 13:37:27 +0530

    changes

--- app.py
+++ app.py
@@ -66,5 +66,3 @@ def summary():
                            transcription=session.get('transcription', ''),
                            summary=session.get('summary', ''))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
